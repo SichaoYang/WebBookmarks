26c8e0eddcf6859d4f20b5201b561213b5d895db
Sichao Yang <32753386+SichaoYang@users.noreply.github.com>
2019-06-04 04:30:06 -0400

    Update and rename ConfigUbuntuSubSys to config_ubuntu.sh

---
+++ config_ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# configure python
+sudo apt update && sudo apt upgrade
+sudo apt install python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 36
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 3
+sudo -H pip install -r requirements.txt
+
+# link to existing Windows folders
+rm -r Documents Pictures
+ln -s /media/sichao/1C228379228356A4/Users/Sichao\ Yang/Documents/ .
+ln -s /media/sichao/1C228379228356A4/Users/Sichao\ Yang/Pictures/ .
